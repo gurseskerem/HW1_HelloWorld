3de5166cd3cf6544c9d0efcb67543f0c1c3b9710
gurseskerem <keremgurses1997@gmail.com>
2021-02-17 00:26:11 +0300

    Initial Commit

---
+++ HW1_HelloWorld/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HW1_HelloWorld
+//
+//  Created by Kerem Gurses on 17.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HW1_HelloWorld/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HW1_HelloWorld
+//
+//  Created by Kerem Gurses on 17.02.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
